32c6284d6dab4f77e569a44d4dc36669ebea55dc
ティマイオス <sh4t98a@gmail.com>
2024-07-27 19:22:17 +0900

    リモートリポジトリの変更

---
+++ src/php01/index02.php
@@ -0,0 +1,7 @@
+$name="Tanaka";
+$last_name="yamada";
+$first_name="Saduro";
+
+echo $name."Jiro"
+each <be/>
+each 
---
+++ src/php01/php02.php
@@ -0,0 +1,11 @@
+<?php
+
+$name="Tanaka";
+$last_name="Yamada";
+$first_name="Saduro";
+
+echo $name."Jiro";
+echo "<br />";
+echo $last_name.$first_name;
+
+?>
